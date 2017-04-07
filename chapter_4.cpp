c6ab9285cda6b57e2e6b249e7015de89361010aa
caogh1 <caogh1@lenovo.com>
2017-04-07 16:06:50 +0800

    add chapter_4.cpp

---
+++ chapter_4.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: chapter_4.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年04月07日 星期五 16时05分22秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout<<"chapter 04 test case"<<endl;
+    return 0;
+}
