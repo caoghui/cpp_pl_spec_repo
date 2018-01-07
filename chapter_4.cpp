448f05211bbe1b708c60b3a9a1c45b2a2d842068
cao_ghui <258214414@qq.com>
2018-01-07 09:32:59 +0800

    update code

--- chapter_4.cpp
+++ chapter_4.cpp
@@ -7,21 +7,33 @@
 
 #include <limits>
 #include <iostream>
-
+#include <vector>
+#include <typeinfo>
+#include <ace/OS.h>
+#include <ace/Log_Msg.h>
 using namespace std;
 
 
 void test_value();
 void test_limits();
-
+void test_auto();
 
 int main(int argc, char** argv)
 {
     cout<<"chapter 04 test case"<<endl;
-    test_limits();
+    //test_limits();
     //test_value();
-    
+    //ACE_TRACE(ACE_TEXT ("main"));
+	test_auto();
     return 0;
+}
+
+void test_auto()
+{
+	vector<int> ivec{1,2,3,4,5};
+	auto len = ivec.size();
+	cout << "auto len = " << typeid(len).name() <<endl;
+
 }
 
 void test_limits()
