d9bf536cbecca87e0e7d0fab6b6db92513a831e0
Bruce <258214414@qq.com>
2018-04-08 18:17:00 +0800

    update file

--- source/chapter_4.cpp
+++ source/chapter_4.cpp
@@ -19,11 +19,35 @@ void test_value();
 void test_limits();
 void test_auto();
 
-struct Sales_data
+class Sales_data
 {
-	string isdn() const { return _bookNo; }
-	string _bookNo;
+	//为Sales_data的非成员函数所做的友元声明
+	friend Sales_data add(const Sales_data&, const Sales_data&);
+	friend istream& read(istream&, Sales_data&);
+	friend ostream& print(ostream&, const Sales_data&);
+
+public:
+	Sales_data() = default;
+	Sales_data(const string& s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p * n) {}
+	Sales_data(const string& s) : bookNo(s) {}
+	Sales_data(istream&);
+
+	string isdn() const { return bookNo; }
+	Sales_data& combine(const Sales_data&);
+	
+private:
+	double avg_price() const
+	{
+		return units_sold ? revenue / units_sold : 0;
+	}
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
 };
+
+Sales_data add(const Sales_data&, const Sales_data&);
+ostream& print(ostream&, const Sales_data&);
+istream& read(istream&, Sales_data&);
 
 int main(int argc, char** argv)
 {
--- source/lambda.cpp
+++ source/lambda.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <boost/config.hpp>
 #ifdef BOOST_NO_CXX11_LAMBDAS
 #   error "lambda functions required"
@@ -40,14 +42,131 @@ void world::add_person(person const& a_person)
     commit = true;
 }
 
+class _functor
+{
+public:
+	int operator()(int x, int y)
+	{
+		return x + y;
+	}
+};
+
+class Tax
+{
+private:
+	float rate;
+	int base;
+public:
+	Tax(float r, int b) : rate(r), base(b) { }
+	float operator()(float money)
+	{
+		return (money - base) * rate;
+	}
+};
+
+vector<int> nums;
+
+void Add(const int val)
+{
+	auto print = [&] {
+		for (auto s : nums)
+		{
+			cout << s << "\t";
+		}
+		cout << endl;
+	};
+
+	//传统的for循环方式
+	for (auto i = nums.begin(); i != nums.end(); ++i)
+	{
+		*i = *i + val;
+	}
+	print();
+
+	//试一试for_each及内置仿函数
+	for_each(nums.begin(), nums.end(), bind2nd(plus<int>(), val));
+	print();
+
+	//实际这里需要使用STL的一个变动性算法: transform
+	transform(nums.begin(), nums.end(), nums.begin(), bind2nd(plus<int>(), val));
+	print();
+
+	//不过在lambda的支持下，还是可以只使用for_each
+	for_each(nums.begin(), nums.end(), [=](int& i) { i += val; })
+		;
+	print();
+}
+
+void test1();
+void test_tax();
+void test_mut();
+void test_add();
+
 int main(int argc, char** argv)
 {
-    cout << "test cxx lambda" << endl;
+	//test1();
+	//test_tax();
+	//test_mut();
+	test_add();
+	/*
     world w;
     person p;
     w.add_person(p);
     BOOST_TEST(w.persons_.size() == 2);
+	*/
     return boost::report_errors();
+}
+
+void test_add()
+{
+	for (int i = 0; i < 10; ++i)
+		nums.push_back(i);
+	Add(10);
+}
+
+void test_mut()
+{
+	int val;
+	//编译失败，在const的lambda中修改常量
+	//auto const_val_lambda = [=]() { val = 3; };
+
+	//非const的lambda，可以修改常量数据
+	auto mutable_val_lambda = [=]() mutable { val = 3; };
+
+	//依然是const的lambda，不过没有改动引用本身
+	auto const_ref_lambda = [&] { val = 3; };
+
+	//依然是const的lambda，通过参数传递val
+	auto const_param_lambda = [&](int v) { v = 3; };
+	const_param_lambda(val);
+}
+
+void test_tax()
+{
+	Tax high(0.4, 30000);
+	Tax middle(0.25, 20000);
+
+	cout << "tax over 3w : " << high(37500) << endl;
+	cout << "tax over 2w : " << middle(27500) << endl;
+	return;
+}
+
+void test1()
+{
+	int girls = 3;
+	int boys = 4;
+
+	//auto totalChild = [](int x, int y) -> int { return x + y; };
+	//auto totalChild = [=]() -> int { return girls + boys; };
+	_functor totalChild;
+	cout << "lambda = " << totalChild(girls, boys) << endl;
+
+	auto totalChild2 = [=, &girls, &boys]() -> int { boys = 5; return girls + boys; };
+	cout << "totalChild2 = " << totalChild2() << endl;
+
+	cout << "girls = " << girls << endl;
+	cout << "boys = " << boys << endl;
+
 }
 
 #endif
